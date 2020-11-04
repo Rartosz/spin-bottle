3c57ab816dc7585e7b1c5eae85c90031c062adc7
Bartosz Gromek <aqaz2000@gmail.com>
2020-11-04 03:51:54 +0100

    Add new files

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let addPlayers8MaxBtn= document.querySelector(".add-classic");
 let delatePlayers8MaxBtn = document.querySelector(".delate-classic");
 let classicGameBtn = document.querySelector(".classic-game-btn");
 let startBtn = document.querySelector(".start-button");
-
 let circle = document.querySelector(".circle");
 
 let polygons = [...document.querySelectorAll(".polygon")];
@@ -49,13 +48,15 @@ let howManyParts = function()
 
     if(players>3)
     {
-        for(let i=0; i<players; i++)
+        
+    polygons[0].style.display="flex";
+    polygons[0].style.transform = `rotate(${0}deg) skewY(${-1*skew}deg)`;
+
+    for(let i=1; i<players; i++)
     {
         polygons[i].style.display="flex";
-      
         polygons[i].style.transform = `rotate(${rotate}deg) skewY(${-1*skew}deg)`;
          rotate=rotate+adrotate;
-        
     }
 
     }
@@ -73,32 +74,120 @@ let howManyParts = function()
             r=180;
         }
     }
+
+    else if(players==3)
+    {
+        let rot=0;
+        let sk = 30;
+        for(let i=0; i<3; i++)
+        {
+            polygons[i].style.display="flex";
+            polygons[i].style.transform = `rotate(${rot}deg) skewY(${sk}deg)`;
+            polygons[i].style.transformOrigin = `0% 100%`;
+            polygons[i].style.width = "70%";
+            polygons[i].style.height = "70%";
+            polygons[i].style.top = "-20%";
+            polygons[i].style.right = "-20%";
+
+            rot=rot+120;
+        }
+    }
     
 }
+
+
+let showEffect = function(bottleRotate) 
+{
+    
+    let result = document.querySelector(".result")
+    let deg = 360/players;
+
+    
+    for(let i=0; i<players; i++)
+    {
+    
+        if(bottleRotate>=(i*deg) && bottleRotate<(deg*(i+1)))
+        {
+            
+            let bgcolor = polygons[i].style.backgroundColor;
+            result.style.backgroundColor = bgcolor;
+            result.textContent = bgcolor;
+            
+            
+        }
+        if(bottleRotate>=(i*deg)+360 && bottleRotate<(deg*(i+1))+360)
+        {
+            
+            let bgcolor = polygons[i].style.backgroundColor;
+            result.style.backgroundColor = bgcolor;
+            result.textContent = bgcolor;
+            
+            
+        }
+        if(bottleRotate>=(i*deg)+720 && bottleRotate<(deg*(i+1))+720)
+        {
+            
+            let bgcolor = polygons[i].style.backgroundColor;
+            result.style.backgroundColor = bgcolor;
+            result.textContent = bgcolor;
+            
+            
+        }
+        if(bottleRotate>=(i*deg)+1080 && bottleRotate<(deg*(i+1))+1080)
+        {
+            
+            let bgcolor = polygons[i].style.backgroundColor;
+            result.style.backgroundColor = bgcolor;
+            result.textContent = bgcolor;
+            
+            
+        }
+        if(bottleRotate>=(i*deg)+1440 && bottleRotate<(deg*(i+1))+1440)
+        {
+            
+            let bgcolor = polygons[i].style.backgroundColor;
+            result.style.backgroundColor = bgcolor;
+            result.textContent = bgcolor;
+            
+            
+        }
+        
+    }
+
+}
+
+
 
 
 
 
 addPlayers8MaxBtn.addEventListener("click", addPlayers8Max);
 delatePlayers8MaxBtn.addEventListener("click", delatePlayers);
-
 classicGameBtn.addEventListener("click", function() 
 {
     let mainMenu = document.querySelector(".main-menu");
     let addUsersClassic = document.querySelector(".add-users-classic");
-
     mainMenu.style.display = "none";
     addUsersClassic.style.display = "flex";
-    
 });
-
 startBtn.addEventListener("click", function() 
 {
     let addUsersClassic = document.querySelector(".add-users-classic");
     addUsersClassic.style.display = "none";
     let classicSpin = document.querySelector(".classic-spin-container");
     classicSpin.style.display = "flex";
-
     howManyParts();
 });
-
+circle.addEventListener("click", function()
+{
+    let bottleRotate = Math.floor(Math.random() * 1800) + 0; 
+    let bottle = document.querySelector(".bottle-spin");
+    bottle.style.transform = `rotate(${bottleRotate}deg)`;
+    let result = document.querySelector(".result");
+    result.style.backgroundColor = "rgba(255,111,2,1)";
+    result.textContent = "color...";
+    setTimeout(function()
+    {
+        showEffect(bottleRotate);
+    },3700);
+});
